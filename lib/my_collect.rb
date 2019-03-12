e4758b4696f336aac764187064c1c544493be025
Daniel Hallada <hallada.daniel@gmail.com>
2019-03-12 22:20:16 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,28 @@
 def my_collect(array)
   i = 0
   new = []
+<<<<<<< HEAD
   while i < array.length
     var = yield array[i]
     new.push(var)
+=======
+  new2 = []
+  while i < array.length
+    yield array[i]
+    new.push(array[i].split(" ").first)
+    i = i + 1
+  end
+  new
+end
+
+def my_collect(array)
+  i = 0
+  new = []
+  new2 = []
+  while i < array.length
+    yield array[i]
+    new.push(array[i].upcase)
+>>>>>>> c0276046213768bc2c51407bb811aa30de803797
     i = i + 1
   end
   new
